f07f7a61679fbec890728da67fbab50d116e93eb
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-02-14 18:38:46 +0100

    Use query option of got

--- src/npms.js
+++ src/npms.js
@@ -1,15 +1,10 @@
 const got = require('got');
 
-const API_VERSION = 2;
+const URL = 'https://api.npms.io/v2/search';
 const RESULT_ITEMS = 10;
-const GOT_OPTIONS = { json: true };
-
-const searchUrl = query => (
-  `https://api.npms.io/v${API_VERSION}/search?q=${query}&size=${RESULT_ITEMS}`
-);
 
 module.exports.search = query => (
-  got(searchUrl(query), GOT_OPTIONS)
+  got(URL, { json: true, query: { q: query, size: RESULT_ITEMS } })
     .then(response => (
       response.body.results.map(result => (
         {
